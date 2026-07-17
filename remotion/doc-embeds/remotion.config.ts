f754aad0d08361586924d4cfa6402a4270c0ce3f
KooshaPari <kooshapari@gmail.com>
2026-07-17 04:23:26 -0700

    recovery: preserve meaningful local work (20260717-0410)

--- remotion/doc-embeds/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-Config.setVideoImageFormat("jpeg");
-Config.setOverwriteOutput(true);
-// public/ is staged per-render by bin/render.mjs.
-Config.setPublicDir("public");
